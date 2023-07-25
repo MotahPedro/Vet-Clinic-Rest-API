a601062f433f878add9dab53391396ee6b456c68
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-07-25 13:20:21 -0300

    BREAKING CHANGE: Adição de @types/express para a atualização dos arquivos em db e middleware

--- db/connect.ts
+++ db/connect.ts
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
+import mongoose, {ConnectOptions} from 'mongoose'
 
-export const connectDB = (url) => {
-  return mongoose.connect(url, {
+export const connectDB = (url:string): Promise<typeof mongoose> => {
+  const options: ConnectOptions = {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
+  }
+  return mongoose.connect(url,options)
 }
