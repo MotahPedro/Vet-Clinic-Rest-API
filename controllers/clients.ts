118ec24e3568476c5b3fb63e4d9e629bea352092
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-07-26 15:53:15 -0300

    fix(routes): Caminho da rota PUT updatePet agora está funcional .

--- controllers/clients.ts
+++ controllers/clients.ts
@@ -34,7 +34,8 @@ export const deleteTutor = async (req:Request,res:Response,next:NextFunction)=>{
 }
 
 export const registerPet = async (req:Request,res:Response, next:NextFunction)=>{
-    const {tutorID}= req.params
+    const {tutorid:tutorID}= req.params
+    console.log(req)
     const tutor = await TutorModel.findById(tutorID)
 
     if(!tutor){
@@ -49,33 +50,33 @@ export const registerPet = async (req:Request,res:Response, next:NextFunction)=>
 }
 
 export const updatePet = async (req:Request,res:Response,next:NextFunction)=>{
-    const {id:tutorID} = req.params
+    const {tutorid:tutorID, petid:petID} = req.params
     const tutor = await TutorModel.findById(tutorID)
-    const {pid:petID} = req.params
-    const pet = await PetModel.findOneAndUpdate({_pid:petID}, req.body,{
+
+    if(!tutor){
+        return next(createCustomError(`Não ha um tutor com id:${tutorID}`, 404))
+    }
+
+    const pet = await PetModel.findOneAndUpdate({_id:petID}, req.body,{
         new: true,
         runValidators: true
     })
+
     if (!pet){
         return next(createCustomError(`O tutor selecionado não tem um pet com id:${petID}`, 404))
     }
-    else if(!tutor){
-        return next(createCustomError(`Não ha um tutor com id:${tutorID}`, 404))
-    }
-    res.status(200).json({tutor})
+    res.status(200).json({pet})
 }
 
-// A forma de validação da existencia de tutor e pet do deletePet atualmente é mais efetiva que a do updatePet. Averiguação assim que tutor é inserido.
 export const deletePet = async (req:Request,res:Response,next:NextFunction)=>{
-    const {id:tutorID} = req.params
+    const {tutorid:tutorID, petid:petID} = req.params
     const tutor = await TutorModel.findById(tutorID)
 
     if(!tutor){
         return next(createCustomError(`Não ha um tutor com id ${tutorID}`, 404))
     }
 
-    const {pid:petID} = req.params
-    const pet = await PetModel.findOneAndDelete({_pid:petID})
+    const pet = await PetModel.findOneAndDelete({_id:petID})
 
     if(!pet){
         return next(createCustomError(`Não ha um pet com o id:${petID} registrado com esse tutor`, 404))
