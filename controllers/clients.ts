2315fd6c8ef55ad4ab9e65f76a1abd55e623dc3a
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-07-25 23:30:07 -0300

    feat(errors): Criação da pasta errors para manusear erros customizados nas rotas do arquivo clients.ts da pasta controllers (3ª tentativa de commitar)

--- controllers/clients.ts
+++ controllers/clients.ts
@@ -1,5 +1,6 @@
 import TutorModel from "../models/clients";
-import { Request, Response} from "express";
+import { Request, Response, NextFunction} from "express";
+import {createCustomError} from '../errors/custom-error'
 
 export const getAllTutors = async (req:Request,res:Response)=>{
     const tutors = await TutorModel.find({})
@@ -9,4 +10,25 @@ export const getAllTutors = async (req:Request,res:Response)=>{
 export const registerTutor = async(req:Request,res:Response)=>{
     const tutor = await TutorModel.create(req.body)
     res.status(201).json({tutor})
+}
+
+export const updateTutor = async (req:Request,res:Response, next:NextFunction)=>{
+    const {id:tutorID} = req.params
+    const tutor = await TutorModel.findOneAndUpdate({_id:tutorID}, req.body,{
+        new: true,
+        runValidators: true
+    })
+    if(!tutor){
+        return next(createCustomError(`Não ha tutores com id: ${tutorID}`,404))
+    }
+    res.status(200).json({tutor})
+}
+
+export const deleteTutor = async (req:Request,res:Response,next:NextFunction)=>{
+    const {id:tutorID} = req.params
+    const tutor = await TutorModel.findOneAndDelete({_id:tutorID})
+    if(!tutor){
+        return next(createCustomError(`Não ha tutores com id: ${tutorID}`,404))
+    }
+    res.status(200).json({tutor})
 }
